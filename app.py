ad45db8fb5d5ef1c2b2800a4aac43573c274cd7d
harinathselvaraj <harinath.selvaraj@outlook.com>
2019-09-19 06:14:11 +0100

    Initial version4

--- app.py
+++ app.py
@@ -21,30 +21,19 @@ def hello():
    return "Hello from my web application."
 
 
-# def get_data():
-#     # engine = create_engine("mysql+pymysql://salutemuser:salutempasswd@localhost/salutemDB")    
-#     engine = create_engine("mysql+pymysql://root:root@localhost/PYTHON_DB")    
-#     drugs = pd.read_sql_query("select * from scraped", engine)
-#    #  drugs['rr_start'] = drugs['rr_start'].apply(pd.to_datetime)
-#    #  drugs['rr_end'] = drugs['rr_end'].apply(pd.to_datetime)
-#    #  drugs['eu_market'] = drugs['eu_market'].apply(pd.to_datetime)
-#    #  drugs['earliest'] = drugs['earliest'].apply(pd.to_datetime)
-#    #  drugs['latest'] = drugs['latest'].apply(pd.to_datetime)
+def get_data():
+    engine = create_engine("mysql+pymysql://twitterusr:twitterpwd@321@166.62.26.1/harrytwitterdb")    
+    twitterdata = pd.read_sql_query("select * from twitterdata", engine)
+   #  drugs['rr_start'] = drugs['rr_start'].apply(pd.to_datetime)
+    # drugs['eu_market'] =  pd.to_datetime(drugs['eu_market'], format='%Y-%m-%d')
+    # drugs['rr_outcome'] = drugs['rr_outcome'].str.lower().str.replace('.','').str.replace('\n','').str.replace('a full','full').str.strip()
 
-#     drugs['rr_start'] =  pd.to_datetime(drugs['rr_start'], format='%Y-%m-%d')
-#     drugs['rr_end'] =  pd.to_datetime(drugs['rr_end'], format='%Y-%m-%d')
-#     drugs['earliest'] =  pd.to_datetime(drugs['earliest'], format='%Y-%m-%d')
-#     drugs['latest'] =  pd.to_datetime(drugs['latest'], format='%Y-%m-%d')
-#     drugs['eu_market'].fillna('2000-01-01', inplace=True)    
-#     drugs['eu_market'] =  pd.to_datetime(drugs['eu_market'], format='%Y-%m-%d')
-#     drugs['rr_outcome'] = drugs['rr_outcome'].str.lower().str.replace('.','').str.replace('\n','').str.replace('a full','full').str.strip()
+    return drugs
 
-#     return drugs
-
-@app.route("/rawdata")
+@app.route("/start")
 def get_dataframe():
-    drugs = get_data()
-    return drugs.head().to_html()
+    twitterdata = get_data()
+    return twitterdata.head().to_html()
 
 if __name__ == '__main__':
     app.run(debug=True)
